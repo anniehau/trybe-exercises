36e6c3ce8c897028879731891659529f11297404
anniehau <anniehaurani@gmail.com>
2021-07-30 18:59:23 -0300

    Adiciona exercicio 2 && exercicio 3.

--- 01-fundamentos/b04-introducao-a-javascript-e-logica-de-programacao/d03-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
+++ 01-fundamentos/b04-introducao-a-javascript-e-logica-de-programacao/d03-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -1,8 +1,8 @@
 // Fazer um triangulo de asteristicos, basicamente uma escadinha
-let n = 10;
+let n = 8;
 let triangulo = [];
 
-//Vamos criar cada elemento da array e colocar um asteristico dentro para cada index
+//Vamos criar cada elemento da array e colocar um asteristico dentro para cada índice, o que forma um triangulo
 for (let i = 0; i < n; i += 1) {
   triangulo.push("*");
   for (let i2 = 1; i2 < (i + 1); i2 += 1) {
@@ -10,6 +10,7 @@ for (let i = 0; i < n; i += 1) {
   }
 }
 
+//Imprime o triângulo
 for (i = 0; i < n; i += 1) {
   console.log(triangulo[i]);
 }
---
+++ 01-fundamentos/b04-introducao-a-javascript-e-logica-de-programacao/d03-javascript-logica-de-programacao-e-algoritmos/exercicio4.js
@@ -0,0 +1,23 @@
+// Fazer uma pirâmide de asteriscos
+let n = 6;
+let triangulo = [];
+
+//Cria a pirâmide usando dois fors: um para o lado esquerdo e outro para o direito
+for (let i = 0; i < n; i += 1) {
+  triangulo.push("");
+  for (let i2 = n - 1; i2 >= 0; i2 -= 1) {
+    if (i2 <= i) {
+      triangulo[i] += ("*");
+    } else {
+      triangulo[i] += (" ");
+    }
+  }
+  for (let i3 = 1; i3 < (i + 1); i3 += 1) {
+    triangulo[i] += ("*");
+  }
+}
+
+//Imprime o triângulo
+for (i = 0; i < n; i += 1) {
+  console.log(triangulo[i]);
+}
