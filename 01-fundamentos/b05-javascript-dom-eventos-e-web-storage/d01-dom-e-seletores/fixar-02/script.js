773c29d605abbe5da7c59011690da6490b3343c9
anniehau <anniehaurani@gmail.com>
2021-08-09 16:02:46 -0300

    Adiciona fixar-02 com exercicio de Admininstrador de Tempo da Trybe completo.

---
+++ 01-fundamentos/b05-javascript-dom-eventos-e-web-storage/d01-dom-e-seletores/fixar-02/script.js
@@ -0,0 +1,13 @@
+document.querySelector("header").style = "background-color: #3ec762";
+
+document.querySelector(".emergency-tasks h3").style = "background-color: purple";
+document.querySelectorAll(".emergency-tasks h3")[1].style = "background-color: purple";
+document.querySelector(".emergency-tasks div").style = "background-color: pink; padding-top: 1px;";
+document.querySelectorAll(".emergency-tasks div")[1].style = "background-color: pink; padding-top: 1px;";
+
+document.querySelector(".no-emergency-tasks h3").style = "background-color: black";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style = "background-color: black";
+document.querySelector(".no-emergency-tasks div").style = "background-color: yellow; padding-top: 1px;";
+document.querySelectorAll(".no-emergency-tasks div")[1].style = "background-color: yellow; padding-top: 1px;";
+
+document.querySelector("footer").style = "background-color: darkgreen";
