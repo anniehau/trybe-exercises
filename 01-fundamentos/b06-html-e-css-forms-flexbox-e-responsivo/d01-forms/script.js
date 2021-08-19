17cf189b9d4e488a86ac6d6c0fc0950b7e84921b
anniehau <anniehaurani@gmail.com>
2021-08-18 21:00:34 -0300

    Adiciona exercicio 1 completo e esqueleto do site com html/css feito.

---
+++ 01-fundamentos/b06-html-e-css-forms-flexbox-e-responsivo/d01-forms/script.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+  function loadStates() {
+    let select = document.querySelector('.form-items select')
+    let states = {
+      'AC': 'Acre',
+      'AL': 'Alagoas',
+      'AP': 'Amapá',
+      'AM': 'Amazonas',
+      'BA': 'Bahia',
+      'CE': 'Ceará',
+      'DF': 'Distrito Federal',
+      'ES': 'Espírito Santo',
+      'GO': 'Goiás',
+      'MA': 'Maranhão',
+      'MT': 'Mato Grosso',
+      'MS': 'Mato Grosso do Sul',
+      'MG': 'Minas Gerais',
+      'PA': 'Pará',
+      'PB': 'Paraíba',
+      'PR': 'Paraná',
+      'PE': 'Pernambuco',
+      'PI': 'Piauí',
+      'RJ': 'Rio de Janeiro',
+      'RN': 'Rio Grande do Norte',
+      'RS': 'Rio Grande do Sul',
+      'RO': 'Rondônia',
+      'RR': 'Roraima',
+      'SC': 'Santa Catarina',
+      'SP': 'São Paulo',
+      'SE': 'Sergipe',
+      'TO': 'Tocantins'
+    }
+
+    for (state in states) {
+      let element = document.createElement('option');
+      element.innerText = states[state];
+      element.value = state;
+      select.appendChild(element);
+    }
+  }
+
+  loadStates();
+}
