c67f183aaa3f917c6372afe6211fa9fa1c2d831d
anniehau <anniehaurani@gmail.com>
2021-08-27 16:15:48 -0300

    Exercicio 2 completo utilizando ternary operator.

--- 01-fundamentos/b07-introducao-a-javascript-es6-e-testes-unitarios/d01-let-const-arrow-functions-e-template-literals/exercicios.js
+++ 01-fundamentos/b07-introducao-a-javascript-es6-e-testes-unitarios/d01-let-const-arrow-functions-e-template-literals/exercicios.js
@@ -9,3 +9,16 @@ function numberFactorial(number) {
 }
 
 console.log(numberFactorial(6));
+
+function largestWord(string) {
+  const wordArray = string.split(' ');
+  let chosenWord = wordArray[0];
+
+  for (let index = 0; index < wordArray.length; index += 1) {
+    const checkWordLength = (wordArray[index].length > chosenWord.length) ? chosenWord = wordArray[index] : '';
+  }
+
+  return chosenWord;
+}
+
+console.log(largestWord('Qual é a maior palavra dessa frase que eu fiz?'))
