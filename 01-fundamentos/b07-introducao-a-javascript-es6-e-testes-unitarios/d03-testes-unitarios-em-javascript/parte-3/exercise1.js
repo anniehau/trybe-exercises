97350d5454493273926ce86ae809358ab829c14c
anniehau <anniehaurani@gmail.com>
2021-08-31 19:45:33 -0300

    Pasta parte-3 adicionada com exercicio 3-2 completo.

--- 01-fundamentos/b07-introducao-a-javascript-es6-e-testes-unitarios/d03-testes-unitarios-em-javascript/parte-3/exercise1.js
+++ 01-fundamentos/b07-introducao-a-javascript-es6-e-testes-unitarios/d03-testes-unitarios-em-javascript/parte-3/exercise1.js
@@ -12,4 +12,5 @@ const greetPeople = (people) => {
 const parameter = ['Irina', 'Ashleigh', 'Elsa'];
 const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
 const output = greetPeople(parameter);
+
 assert.deepStrictEqual(output, result);
