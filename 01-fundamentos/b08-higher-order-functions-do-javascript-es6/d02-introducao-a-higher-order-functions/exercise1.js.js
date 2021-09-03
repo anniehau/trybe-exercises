d31aa9d0367fa8952664e2103f91161636a2139b
anniehau <anniehaurani@gmail.com>
2021-09-03 19:05:21 -0300

    Exercicio 3 completo.

--- 01-fundamentos/b08-higher-order-functions-do-javascript-es6/d02-introducao-a-higher-order-functions/exercise1.js.js
+++ 01-fundamentos/b08-higher-order-functions-do-javascript-es6/d02-introducao-a-higher-order-functions/exercise1.js.js
@@ -83,3 +83,20 @@ function smallerName() {
 }
 
 assert.strictEqual(smallerName(), 'Duna');
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  return books.find((element) => element.name.length === 26);
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
