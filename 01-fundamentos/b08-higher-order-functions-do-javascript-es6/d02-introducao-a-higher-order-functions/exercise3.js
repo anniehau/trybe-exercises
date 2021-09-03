3debd0f898a06b81394e28a98d4280c3f46b9a55
anniehau <anniehaurani@gmail.com>
2021-09-03 19:14:09 -0300

    Arruma codigo do exercise3.js adicionando expectedResult.

--- 01-fundamentos/b08-higher-order-functions-do-javascript-es6/d02-introducao-a-higher-order-functions/exercise3.js
+++ 01-fundamentos/b08-higher-order-functions-do-javascript-es6/d02-introducao-a-higher-order-functions/exercise3.js
@@ -63,6 +63,17 @@ const books = [
   },
 ];
 
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
 function getNamedBook() {
   return books.find((element) => element.name.length === 26);
 }
