11b591110ab52a85e32530f5a14d0d8f3d7eb64b
anniehau <anniehaurani@gmail.com>
2021-09-24 02:19:29 -0300

    Adiciona exercicio 4 completo.

--- 01-fundamentos/10-testes-automatizados-com-jest/3-simulando-comportamentos/exercises/4-5.test.js
+++ 01-fundamentos/10-testes-automatizados-com-jest/3-simulando-comportamentos/exercises/4-5.test.js
@@ -1,28 +1,40 @@
 const string = require('./4-5');
 
 // Exercicio 4
-describe('Exercicio 4 - Teste de mock com as funções de string', () => {
+describe('Teste de mock retornando à funcionalidade original após', () => {
   test('Função uppercase retorna tudo em lowercase', () => {
-    string.uppercase = jest.fn()
+    const mockUppercase = jest.spyOn(string, 'uppercase')
     .mockImplementation((str) => str.toLowerCase());
   
     expect(string.uppercase('TESTE')).toBe('teste');
     expect(string.uppercase).toHaveBeenCalled();
+
+    string.uppercase.mockRestore();
+    expect(string.uppercase('teste')).toBe('TESTE');
+    expect(string.uppercase).toHaveBeenCalledTimes(1);
   })
   
   test('Função first retorna o último caractére', () => {
-    string.first = jest.fn()
+    const mockFirst =  jest.spyOn(string, 'first')
     .mockImplementation((str) => str[str.length-1]);
     
     expect(string.first('teste')).toBe('e');
     expect(string.first).toHaveBeenCalled();
+
+    string.first.mockRestore();
+    expect(string.first('teste')).toBe('t');
+    expect(string.first).toHaveBeenCalledTimes(1);
   })
   
   test('Função fuse funde 3 strings', () => {
-    string.fuse = jest.fn()
+    const mockFuse =  jest.spyOn(string, 'fuse')
     .mockImplementation((str1, str2, str3) => str1 + str2 + str3);
     
     expect(string.fuse('tes', 't', 'e')).toBe('teste');
     expect(string.fuse).toHaveBeenCalled();
+
+    string.fuse.mockRestore();
+    expect(string.fuse('tes', 'te')).toBe('teste');
+    expect(string.fuse).toHaveBeenCalledTimes(1);
   })
 })
